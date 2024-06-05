805b307bb7f3c59264f5119cdb977f0669861e84
Ferhan Ismailov <floorer@gmail.com>
2024-06-06 00:03:41 +0300

    Fix RTK Query and Redux providers

--- app/store.ts
+++ app/store.ts
@@ -1,6 +1,14 @@
 import {configureStore} from "@reduxjs/toolkit"
+import { userApi } from "../features/userSlice";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 
 export const store = configureStore({
-    reducer: {},
-});
+    reducer: {
+        [userApi.reducerPath]: userApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(userApi.middleware),
+  })
+  
+  setupListeners(store.dispatch)
--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,11 @@ import { RouterProvider} from 'react-router-dom'
 import { router } from '../router.js'
 import { store }  from "../app/store.js"
 import { Provider } from "react-redux"
-import { ApiProvider } from '@reduxjs/toolkit/query/react'
-import { userApi } from '../features/userSlice.js'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ApiProvider api={userApi}>
         <RouterProvider router={router} />
-      </ApiProvider>
     </Provider>
   </React.StrictMode>,
 )
